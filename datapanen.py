f8b3774e56a190cbabbd69d956fc68779ab7a8b2
Andaridp13 <andari.dela@mhs.itenas.ac.id>
2024-12-03 19:15:22 +0700

    Soal 2

--- datapanen.py
+++ datapanen.py
@@ -49,3 +49,7 @@ for lokasi, data in data_panen.items():
     for jenis, jumlah in data['hasil_panen'].items():
         print(f"  {jenis.capitalize()}: {jumlah}")
     print()
+    
+#Tampilkan jumlah hasil panen jagung dari lokasi2
+print("Jumlah hasil panen jagung dari lokasi2:",
+      data_panen['lokasi2']['hasil_panen']['jagung'])
